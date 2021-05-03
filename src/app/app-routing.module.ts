12175b73ba5ff979056a4c568e7e4c9aacc20321
adcottle <cottle.amanda1@gmail.com>
2021-05-02 21:39:23 -0400

    app routing to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 /*Customization Modules*/
 import { MaterialModule } from './material.component';
 
+/*App Pages*/
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
