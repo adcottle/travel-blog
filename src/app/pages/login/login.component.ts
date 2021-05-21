098138057057b3c96edfad1c76a2d10fc648a0df
adcottle <cottle.amanda1@gmail.com>
2021-05-21 17:03:46 -0400

    added responsiveness

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,6 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  imageObject = [{
+    image: '../../../assets/pics/navajo_bridge.JPG',
+    thumbImage: '../../../assets/pics/navajo_bridge.JPG',
+    title: 'Navajo Bridge, Marble Canyon'
+}, {
+    image: '../../../assets/pics/forest_gump.jpg',
+    thumbImage: '../../../assets/pics/forest_gump.jpg',
+    title: "I'll go home now",
+}, {
+    image: '../../../assets/pics/cliff.jpg',
+    thumbImage:  '../../../assets/pics/cliff.jpg',
+    title: "Looking over a scary cliff"
+},{
+    image: '../../../assets/pics/camelback.jpg',
+    thumbImage: '../../../assets/pics/camelback.jpg',
+    title: 'Camelback Mountain Phoenix'
+}, {
+    image: '../../../assets/pics/red_rocks.jpg',
+    thumbImage: '../../../assets/pics/red_rocks.jpg',
+    title: 'Red Rocks of Sedona'
+}, {
+    image: '../../../assets/pics/goblin_valley.JPG',
+    thumbImage: '../../../assets/pics/goblin_valley.JPG',
+    title: 'Goblin Valley'
+}];
   constructor() { }
 
   ngOnInit(): void {
