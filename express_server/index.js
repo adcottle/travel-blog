1dfc84eabf605fab09b297550ea0677db75b3f2f
adcottle <cottle.amanda1@gmail.com>
2021-06-21 21:07:17 -0400

    removed unused params

--- express_server/index.js
+++ express_server/index.js
@@ -3,11 +3,44 @@ const express = require('express');
 const upload = require("./middlewares/gridfs");
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const fs  = require('fs');
 const Grid = require('gridfs-stream');
 
-
+// Express settings
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cors());
+
+//Express APIs
+const trips = require('./routes/trip.routes')
+const users = require('./routes/auth.routes')
+
+
+//MongoDB conection
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Database connected')
+},
+    error => {
+        console.log("Database can't be connected: " + error)
+    }
+)
+
+// Remvoe MongoDB warning error
+mongoose.set('useCreateIndex', true);
+
+
+// // Serve static resources
+// app.use('/public', express.static('public'));
+
+app.use('/trips', trips)
+app.use('/users', users)
 
 app.set('view engine','ejs');
 
@@ -75,63 +108,6 @@ gfs.files.find().toArray((err, files) =>{
         });
     });
 
-const port=3000;
-
+// Define PORT
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`server started on port ${port}`));
-
-// const express = require('express');
-// const mongoose = require('mongoose');
-const cors = require('cors');
-const dbConfig = require('./database/db');
-
-// // Express APIs
-const trips = require('./routes/trip.routes')
-const users = require('./routes/auth.routes')
-
-
-// // Express APIs
-// const api = require('./routes/auth.routes');
-
-// MongoDB conection
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Database connected')
-},
-    error => {
-        console.log("Database can't be connected: " + error)
-    }
-)
-
-// Remvoe MongoDB warning error
-mongoose.set('useCreateIndex', true);
-
-
-// // Express settings
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({
-//     extended: false
-// }));
-app.use(cors());
-
-// // Serve static resources
-// app.use('/public', express.static('public'));
-
-app.use('/trips', trips)
-app.use('/users', users)
-
-// // Define PORT
-// const port = process.env.PORT || 4000;
-// const server = app.listen(port, () => {
-//     console.log('Connected to port ' + port)
-// })
-
-// // Express error handling
-// app.use((req, res, next) => {
-//     setImmediate(() => {
-//         next(new Error('Something went wrong'));
-//     });
-// });
